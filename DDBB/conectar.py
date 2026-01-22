7452ec45f900f8e9697f10740ca11dd4537ec728
MikeSrz <onriosrz@gmail.com>
2026-01-22 17:46:42 +0100

    Editando el fichero de conexion

--- DDBB/conectar.py
+++ DDBB/conectar.py
@@ -1,13 +1,19 @@
 import mysql.connector
+
+"""
+CONEXION CON BASE DE DATOS, EDITA LOS PARÁMETROS DE CONEXIÓN.
+"""
 def conectar(db_name):
+    config = {
+        'host':'localhost',
+        'port':777,
+        'user':'root',
+        'password':'',
+        'database':'db_name'
+    }
+
     try:
-        db = mysql.connector.connect(
-            host="localhost",
-            port=7777,
-            user="root",
-            password="",
-            database = db_name
-        )
+        db = mysql.connector.connect(**config) #    ** sirve para pasar parametros como: host=tikitiki, port=rikiriki...
         if db.is_connected():
             print("Conexion con exito a la BBDD")
             return db;
@@ -16,5 +22,5 @@ def conectar(db_name):
     except Exception as e:
         print(f"No conectado: {e}")
 if __name__ == "__main__":
-    db = conectar()
+    db = conectar("world")
     db.close()
